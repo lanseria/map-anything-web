02925b25cd74e38c211046e2cc18a1e26604145d
lanseria <zhangchao564265135@hotmail.com>
2023-05-11 21:36:02 +0800

    update add geo search

--- src/components/Collapse/CollapeData.vue
+++ src/components/Collapse/CollapeData.vue
@@ -9,16 +9,19 @@ const videoList = computed(() => {
 const handleClick = (item: FormatVideo) => {
   globalVideoId.value = item.aid
 }
+const handlePlay = (item: FormatVideo) => {
+  open('https://www.bilibili.com/video/' + item.bvid)
+}
 </script>
 <template>
   <a-scrollbar style="height:200px;overflow: auto;">
     <div class="grid grid-cols-2 gap-20px">
       <div v-for="item in videoList" :key="item.aid" class="flex rounded-lg p-5px border border-[var(--color-neutral-2)]"
-        :class="globalVideoId !== item.aid ? 'bg-[var(--color-neutral-2)] cursor-pointer' : 'bg-blue-500 text-white'"
+        :class="globalVideoId !== item.aid ? 'bg-[var(--color-neutral-2)]' : 'bg-blue-500 text-white'"
         @click="handleClick(item)">
         <div class="flex flex-col">
           <div>{{ item.title }}</div>
-          <div title="查看数"><icon-eye />{{ item.play }}</div>
+          <div class="cursor-pointer" title="查看数" @click="handlePlay(item)"><icon-eye />{{ item.play }}</div>
           <div title="评论数"><icon-message />{{ item.comment }}</div>
           <div title="发布时间"><icon-clock-circle />{{ item.createdStr }}</div>
         </div>
--- src/components/Content/ContentMap.vue
+++ src/components/Content/ContentMap.vue
@@ -3,8 +3,10 @@ import type { LngLatLike } from 'mapbox-gl'
 import mapboxgl from 'mapbox-gl'
 import MapboxDraw from '@mapbox/mapbox-gl-draw'
 import MapboxLanguage from '@mapbox/mapbox-gl-language'
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import 'mapbox-gl/dist/mapbox-gl.css'
 import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 import DrawLineString from '~/composables/draw/linestring'
 import drawStyles from '~/composables/draw/styles'
@@ -43,6 +45,18 @@ onMounted(() => {
   window.draw = draw
 
   map.addControl(new mapboxgl.NavigationControl())
+  map.addControl(
+    new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+      countries: 'CN',
+      types: 'poi,district,place',
+      language: 'zh-hans',
+      // @ts-ignore
+      worldview: 'cn'
+    })
+    , 'top-left'
+  );
   map.addControl(draw)
   map.on('load', () => {
     setTimeout(() => {
@@ -66,11 +80,10 @@ onMounted(() => {
 
 <template>
   <div class="h-full w-full relative">
-    <div
-      ref="mapContainer"
-      class="h-full w-full relative"
-    />
-    <div v-if="globalIsMapboxLoad" class="text-[var(--color-neutral-10)] absolute left-0 bottom-1/2 px-1 py-4 bg-white dark:bg-black cursor-pointer  z-10" @click="handleContentSideCollapsed()">
+    <div ref="mapContainer" class="h-full w-full relative" />
+    <div v-if="globalIsMapboxLoad"
+      class="text-[var(--color-neutral-10)] absolute left-0 bottom-1/2 px-1 py-4 bg-white dark:bg-black cursor-pointer  z-10"
+      @click="handleContentSideCollapsed()">
       <div v-if="!storeContentSideCollapsed" class="i-carbon:caret-right" />
       <div v-else class="i-carbon:caret-left" />
     </div>
