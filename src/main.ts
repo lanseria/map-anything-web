0fb697c883a0d923d8b8b3d4f6bc117bf911fcea
lanseria <zhangchao564265135@hotmail.com>
2023-06-20 08:50:39 +0800

    add plausible

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import routes from 'virtual:generated-pages'
 import ArcoVue from '@arco-design/web-vue'
 import ArcoVueIcon from '@arco-design/web-vue/es/icon'
-import baiduAnalytics from 'vue-baidu-analytics'
 import App from './App.vue'
 import './worker'
 
@@ -18,17 +17,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
-if (import.meta.env.PROD) {
-  app
-  // 启动插件
-    .use(baiduAnalytics, {
-      router,
-      siteIdList: [
-        '48f69cc7e08b3b8a1bc58d8103ecfcc2',
-      ],
-      isDebug: false,
-    })
-}
 app.use(ArcoVue, {})
 app.use(ArcoVueIcon)
 app.use(router)
